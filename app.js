b11d8e10eca549cc728f6ad00cd182277337e681
MaciejMM <maciej.myslinski88@gmail.com>
2020-07-29 13:20:47 +0200

    Fixing error res
    Fixing error response for 404

--- app.js
+++ app.js
@@ -14,17 +14,17 @@ app.use('/products', productRoutes)
 app.use('/orders', ordersRoutes)
 
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     const error = new Error('Not found');
-    error.status(404);
+    error.status = 404;
     next(error);
 })
 
-app.use((error, req,res,next)=>{
+app.use((error, req, res, next) => {
     res.status(error.status || 500)
     res.json({
-        error:{
-            message:error.message
+        error: {
+            message: error.message
         }
     });
 })
