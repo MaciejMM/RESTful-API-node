b967a690c37697a0444a2414274a88b7a1132527
MaciejMM <maciej.myslinski88@gmail.com>
2020-08-01 12:19:25 +0200

    Update methods
    Includes all neccessary methods with proper routes connections

--- app.js
+++ app.js
@@ -5,46 +5,57 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const process = require('./nodemon.json')
 
+
 const mongoUrl = `mongodb+srv://node-rest-shop:${process.env.MONGO_ATLAS_PW}@node-rest-shop.erlnq.mongodb.net/node-rest-shop?retryWrites=true&w=majority`;
 
 const productRoutes = require('./api/routes/products')
-const ordersRoutes = require('./api/routes/order')
+const orderRoutes = require('./api/routes/order')
 
 mongoose.connect(mongoUrl,{
     useNewUrlParser: true, 
     useUnifiedTopology: true
 })
+// 0: disconnected
+// 1: connected
+// 2: connecting
+// 3: disconnecting
 
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allowed-Methods', 'PUT, POST, PATCH, DELETE, GET')
-    }
 
-})
-// Routes for handling request
-app.use('/products', productRoutes)
-app.use('/orders', ordersRoutes)
-
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error);
-})
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+// Routes which should handle requests
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500)
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
-})
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
